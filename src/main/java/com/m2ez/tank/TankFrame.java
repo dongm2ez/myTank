2763ad4a80e1292fb7b7ea5c9075684af95207ab
David Happ <dongm2ez@163.com>
2021-02-05 15:15:34 +0800

    增加双缓冲方法，去除画面闪烁

--- src/main/java/com/m2ez/tank/TankFrame.java
+++ src/main/java/com/m2ez/tank/TankFrame.java
@@ -11,8 +11,10 @@ public class TankFrame extends Frame {
     Tank myTank = new Tank(200, 200, Dir.DOWN);
     Bullet myBullet = new Bullet(300, 300, Dir.DOWN);
 
+    private static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
+
     public TankFrame() {
-        setSize(800, 600);
+        setSize(GAME_WIDTH, GAME_HEIGHT);
         setResizable(false);
         setTitle("tank war");
         setVisible(true);
@@ -25,8 +27,22 @@ public class TankFrame extends Frame {
                 System.exit(0);
             }
         });
+    }
 
+    Image offScreenImage = null;
 
+    @Override
+    public void update(Graphics g) {
+        if (offScreenImage == null) {
+            offScreenImage = createImage(GAME_WIDTH, GAME_HEIGHT);
+        }
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.BLACK);
+        gOffScreen.fillRect(0,0,GAME_WIDTH, GAME_HEIGHT);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage, 0, 0, null);
     }
 
     @Override
@@ -99,14 +115,14 @@ public class TankFrame extends Frame {
 
         private void setMainTankDir() {
             if (!bL && !bU && !bR && !bD) myTank.setMoving(false);
-            else myTank.setMoving(true);
+            else {
+                myTank.setMoving(true);
 
-            if (bL) myTank.setDir(Dir.LEFT);
-            if (bU) myTank.setDir(Dir.UP);
-            if (bR) myTank.setDir(Dir.RIGHT);
-            if (bD) myTank.setDir(Dir.DOWN);
-
-
+                if (bL) myTank.setDir(Dir.LEFT);
+                if (bU) myTank.setDir(Dir.UP);
+                if (bR) myTank.setDir(Dir.RIGHT);
+                if (bD) myTank.setDir(Dir.DOWN);
+            }
         }
 
     }
