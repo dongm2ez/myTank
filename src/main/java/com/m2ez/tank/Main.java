d1bcaec7cb92ec82750b3313feac1b11cad7687b
David Happ <dongm2ez@163.com>
2021-02-06 11:35:07 +0800

    添加配置文件，初始化坦克数量从配置文件读取

--- src/main/java/com/m2ez/tank/Main.java
+++ src/main/java/com/m2ez/tank/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         TankFrame tf = new TankFrame();
 
-        for (int i = 0; i < 5; i++) {
+        int initTankCount = Integer.parseInt((String) PropertyMgr.get("initTankCont")) ;
+        for (int i = 0; i < initTankCount; i++) {
             tf.tanks.add(new Tank(50 + i * 80, 200, Dir.DOWN, Group.BAD, tf));
         }
 
---
+++ src/main/java/com/m2ez/tank/PropertyMgr.java
@@ -0,0 +1,21 @@
+package com.m2ez.tank;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyMgr {
+    static Properties properties = new Properties();
+
+    static {
+        try {
+            properties.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Object get(String key) {
+        if (properties == null) return null;
+        return properties.get(key);
+    }
+}
